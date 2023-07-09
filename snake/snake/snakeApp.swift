da8f7f879eaa3090cd2f3c2952edddd2100e68f6
AndyyHuang <handy1221@berkeley.edu>
2023-07-09 19:00:46 -0400

    project created

---
+++ snake/snake/snakeApp.swift
@@ -0,0 +1,17 @@
+//
+//  snakeApp.swift
+//  snake
+//
+//  Created by Andy Huang on 7/9/23.
+//
+
+import SwiftUI
+
+@main
+struct snakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
